5a7c9b7029cc58ebf65f172eb044448721f026f3
iamquiroz <jquiroz.mexicali@gmail.com>
2021-03-12 19:26:32 -0800

    navbar

---
+++ seoBlog/frontend/pages/_app.js
@@ -0,0 +1,18 @@
+//page/_app.js
+
+import Head from 'next/head'
+
+function MyApp ({ Component, pageProps }) {
+
+  return (
+    <>
+      <Head>
+	<meta charSet='UTF-8' />
+	<meta name='viewport' content='width=device=width, initial-scale=1.0' />
+      </Head>
+      <Component {...pageProps } />
+    </>
+  )
+}
+
+export default MyApp;
