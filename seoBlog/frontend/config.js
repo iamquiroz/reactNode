b2c8b710893a62bf100c3f9324c1832202ddd51f
iamquiroz <jquiroz.mexicali@gmail.com>
2021-03-13 00:25:46 -0800

    Signup action

--- seoBlog/frontend/config.js
+++ seoBlog/frontend/config.js
@@ -3,6 +3,6 @@ import getConfig from 'next/config';
 const {publicRuntimeConfig} = getConfig();
 
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://seoblog.com' : 'http://localhost:5000';
+export const API = publicRuntimeConfig.PRODUCTION ? 'https://seoblog.com' : 'http://localhost:5000/api';
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
