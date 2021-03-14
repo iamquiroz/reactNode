21ab37d337f4110bacb3045ba283ab84b00f735e
iamquiroz <jquiroz.mexicali@gmail.com>
2021-03-13 16:46:59 -0800

    Auth and admin middlewares - Backend

--- seoBlog/backend/controllers/auth.js
+++ seoBlog/backend/controllers/auth.js
@@ -3,6 +3,7 @@ const shortId = require('shortid');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt')
 
+
 exports.signup = (req,res) => {
 
     User.findOne({email: req.body.email}).exec((err, user) => {
@@ -83,3 +84,50 @@ exports.requireSignin = expressJwt ({
     secret: process.env.JWT_SECRET,
     algorithms: ['HS256']
 });
+
+exports.authMiddleware = (req, res, next) => {
+    const authUserId = req.user._id
+    User.findById({_id: authUserId}).exec((err,user) => {
+	if(err || !user) {
+	    return res.status(400).json({
+		error: 'User not found' 
+	    })	
+	}
+	req.profile = user
+	next()
+    });
+}
+
+exports.adminMiddleware = (req,res,next) => {
+    const adminUserId = req.user._id
+    User.findById({_id: adminUserId}).exec((err,user) => {
+	if(err || !user){
+	    return res.status(400).json({
+		error: 'User not found' 
+	    })
+	}
+
+	if(user.role !== 1){
+	    return res.status(400).json({
+	      error: 'Admin resource. Access denied'
+	    })
+	}
+	req.profile = user
+	next()
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- seoBlog/backend/server.js
+++ seoBlog/backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 //bring routes
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
-
+const userRoutes = require('./routes/user');
 
 //app
 const app = express();
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'development') {
 // routes middlewares
 app.use('/api',blogRoutes);
 app.use('/api',authRoutes);
-
+app.use('/api/',userRoutes);
 // port
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
